7d8caa31d3bbe611f74f16be125e8e9f5e9be321
bruno-wolf <bruno.m.lobato@gmail.com>
2020-10-27 19:10:13 +0000

    backend: added 4 routes, one for each type

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,35 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-	return response.json({ message: 'hello wolf' });
+app.get('/projects', (request, response) => {
+	return response.json([
+		'projeto 1',
+		'projeto 2'
+	]);
+});
+
+app.post('/projects', (request, response) => {
+	return response.json([
+		'projeto 1',
+		'projeto 2',
+		'projeto 3'
+	]);
+});
+
+// http://localhost/projects/2
+app.put('/projects/:id', (request, response) => {
+	return response.json([
+		'projeto 4',
+		'projeto 2',
+		'projeto 3'
+	]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+	return response.json([
+		'projeto 2',
+		'projeto 3'
+	]);
 });
 
 app.listen(3333, () => {
