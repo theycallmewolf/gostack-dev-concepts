934c20360ac1d9538da694789ebb6eb6f202e8c0
bruno-wolf <bruno.m.lobato@gmail.com>
2020-10-30 19:59:03 +0000

    mobile: code formatting with prettier

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const { v4: uuid, validate: validateUuid } = require('uuid');
+const express = require("express");
+const cors = require("cors");
+const { v4: uuid, validate: validateUuid } = require("uuid");
 
 const app = express();
 app.use(cors());
@@ -10,85 +10,85 @@ const projects = [];
 
 // middleware
 function logRequests(request, response, next) {
-	const { method, url } = request;
-	const logLabel = `[${method.toUpperCase()}] ${url}`;
+  const { method, url } = request;
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
 
-	console.time(logLabel);
+  console.time(logLabel);
 
-	next(); // next middleware
+  next(); // next middleware
 
-	console.timeEnd(logLabel);
+  console.timeEnd(logLabel);
 }
 
 function validateProjectID(request, response, next) {
-	const { id } = request.params;
+  const { id } = request.params;
 
-	if (!validateUuid(id)) {
-		return response.status(400).json({ error: 'invalid project ID' });
-	}
+  if (!validateUuid(id)) {
+    return response.status(400).json({ error: "invalid project ID" });
+  }
 
-	return next();
+  return next();
 }
 
 app.use(logRequests);
-app.use('/projects/:id', validateProjectID);
+app.use("/projects/:id", validateProjectID);
 
 // routes
-app.get('/projects', (request, response) => {
-	const { title } = request.query;
-	const results =
-		title ? projects.filter((project) => project.title.includes(title)) :
-		projects;
+app.get("/projects", (request, response) => {
+  const { title } = request.query;
+  const results = title
+    ? projects.filter((project) => project.title.includes(title))
+    : projects;
 
-	return response.json(results);
+  return response.json(results);
 });
 
-app.post('/projects', (request, response) => {
-	const { title, owner } = request.body;
+app.post("/projects", (request, response) => {
+  const { title, owner } = request.body;
 
-	const project = { id: uuid(), title, owner };
+  const project = { id: uuid(), title, owner };
 
-	projects.push(project);
+  projects.push(project);
 
-	return response.json(project);
+  return response.json(project);
 });
 
 // http://localhost/projects/2
-app.put('/projects/:id', (request, response) => {
-	const { id } = request.params;
-	const { title, owner } = request.body;
+app.put("/projects/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
 
-	const projectIndex = projects.findIndex((project) => project.id === id);
+  const projectIndex = projects.findIndex((project) => project.id === id);
 
-	if (projectIndex < 0) {
-		return response.status(400).json({ error: 'project not found' });
-	}
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "project not found" });
+  }
 
-	const project = {
-		id,
-		title,
-		owner
-	};
+  const project = {
+    id,
+    title,
+    owner,
+  };
 
-	projects[projectIndex] = project;
+  projects[projectIndex] = project;
 
-	return response.json(project);
+  return response.json(project);
 });
 
-app.delete('/projects/:id', (request, response) => {
-	const { id } = request.params;
+app.delete("/projects/:id", (request, response) => {
+  const { id } = request.params;
 
-	const projectIndex = projects.findIndex((project) => project.id === id);
+  const projectIndex = projects.findIndex((project) => project.id === id);
 
-	if (projectIndex < 0) {
-		return response.status(400).json({ error: 'project not found' });
-	}
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "project not found" });
+  }
 
-	projects.splice(projectIndex, 1);
+  projects.splice(projectIndex, 1);
 
-	return response.status(204).send();
+  return response.status(204).send();
 });
 
 app.listen(3333, () => {
-	console.log('back-end start 🐺🤟');
+  console.log("back-end start 🐺🤟");
 });
--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,28 +1,46 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from "react-native";
+import api from "./services/api";
 
 export default function App() {
-	return (
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get("projects").then((response) => {
+      console.log(response.data);
+      setProjects(response.data);
+    });
+  }, []);
+
+  return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" translucent />
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello Wolf!</Text>
-      </View>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="#7159c1"
+        translucent
+      />
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={project => project.id }
+          renderItem={({ item })=>(
+            <Text style={styles.title}>{item.title}</Text>
+          )}
+          />
+      </SafeAreaView>
     </>
-	);
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		backgroundColor: '#7159c1',
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center'
-	},
-	title: {
-		color: '#fff',
-		fontSize: 40,
-		fontWeight: 'bold',
-		letterSpacing: 4
-	}
+  container: {
+    backgroundColor: "#7159c1",
+    flex: 1,
+  },
+  title: {
+    color: "yellow",
+    fontSize: 30,
+    fontWeight: "bold",
+    letterSpacing: 2,
+  },
 });
