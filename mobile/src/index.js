e0cd4a3a92e4e785182f748a289dc1064db69951
bruno-wolf <bruno.m.lobato@gmail.com>
2020-10-30 18:24:01 +0000

    mobile: testing basic concepts

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,6 +1,28 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 
-export default function App(){
-  return <View/>
+export default function App() {
+	return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" translucent />
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello Wolf!</Text>
+      </View>
+    </>
+	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#7159c1',
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	title: {
+		color: '#fff',
+		fontSize: 40,
+		fontWeight: 'bold',
+		letterSpacing: 4
+	}
+});
