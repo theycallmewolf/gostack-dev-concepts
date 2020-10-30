9483bb6cf86ee851c34f776cb20f416a4bb83810
bruno-wolf <bruno.m.lobato@gmail.com>
2020-10-30 19:57:56 +0000

    frontend: code formatting with prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import api from './services/api';
-import Header from './components/Header';
-import './App.css';
-import backgroundImage from './assets/background.jpg';
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
+import Header from "./components/Header";
+import "./App.css";
+import backgroundImage from "./assets/background.jpg";
 
 function App() {
+  const [projects, setProjects] = useState([]);
 
-	const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    api.get("/projects").then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
 
-	useEffect(() => {
-		api.get('/projects').then(response => {
-			setProjects(response.data);
-		})
-	}, []);
+  async function handleAddProject() {
+    // setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    const response = await api.post("/projects", {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Lobatinni",
+    });
 
-	async function handleAddProject() {
-		// setProjects([...projects, `Novo projeto ${Date.now()}`]);
-		const response = await api.post('/projects', {
-			title: `Novo projeto ${Date.now()}`,
-			owner: 'Lobatinni',
-		});
+    const project = response.data;
+    setProjects([...projects, project]);
+  }
 
-		const project = response.data;
-		setProjects([...projects, project]);
+  return (
+    <>
+      <Header title="Projects" />
 
-	}
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id}>{project.title}</li>
+        ))}
+      </ul>
 
-	return (
-		<>
-			<Header title="Projects" />
+      <button type="button" onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
 
-			<ul>
-				{projects.map(project => <li key={project.id}>{project.title}</li>)}
-			</ul>
-
-			<button type="button" onClick={handleAddProject}>Adicionar projeto</button>
-
-			<br />
-			{/* <img width={300} src={ backgroundImage }/> */}
-		</>
-	);
+      <br />
+      {/* <img width={300} src={ backgroundImage }/> */}
+    </>
+  );
 }
 
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
-export default function Header({title}) {
-	return (
-		<header>
-			<h1>{ title }</h1>
-		</header>
-	);
+export default function Header({ title }) {
+  return (
+    <header>
+      <h1>{title}</h1>
+    </header>
+  );
 }
